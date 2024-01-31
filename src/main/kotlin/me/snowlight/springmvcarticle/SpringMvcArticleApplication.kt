1b38b35616642979d1fa87defd72ad99ae817982
snowlight-aemt <it.s.aemt@gmail.com>
2024-01-31 22:00:48 +0900

    Init Spring MVCOD

---
+++ src/main/kotlin/me/snowlight/springmvcarticle/SpringMvcArticleApplication.kt
@@ -0,0 +1,11 @@
+package me.snowlight.springmvcarticle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcArticleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcArticleApplication>(*args)
+}
---
+++ src/test/kotlin/me/snowlight/springmvcarticle/SpringMvcArticleApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.snowlight.springmvcarticle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMvcArticleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
