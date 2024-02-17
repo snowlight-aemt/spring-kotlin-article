909c8ba0125a12be25665d30d4647d933a9b6181
snowlight-aemt <it.s.aemt@gmail.com>
2024-02-18 00:47:33 +0900

    Mariadb 테스트 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/me/snowlight/springmvcarticle/model/Article.kt
+++ src/main/kotlin/me/snowlight/springmvcarticle/model/Article.kt
@@ -6,12 +6,14 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.Table
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @Entity
+@Table(name = "TB_ARTICLE")
 class Article (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0,
